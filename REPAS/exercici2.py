e806193b764fd05ecebd2547e54a8cc6165b76aa
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2024-01-09 19:57:02 +0100

    subida 1

---
+++ REPAS/exercici2.py
@@ -0,0 +1,15 @@
+
+valor = input('Introdueix un valor: ')
+
+
+iva = input('Introdueix un iva: ')
+
+div = {iva} / 100
+
+suma = 1+{div}
+valor_final = {valor}*{suma}
+
+
+print('El valor inicial es: {valor}')
+print('El iva es: {iva}')
+print('El valor final es: {valor_final}')
---
+++ REPAS/exercici3.py
@@ -0,0 +1,10 @@
+valor1=input('Introdueix un valor: ')
+
+valor2=input('Introdueix el altre valor: ')
+
+if valor1 > valor2:
+     print(valor1 + ' es mes gran')
+else:
+     print(valor2 + ' es mes gran')
+  
+    
