9e70d66b4d13ee0cdee4b6b1ac64542cd8873f44
Aleksandra <2023_Aleksandra.Jovanovic@iticbcn.cat>
2024-01-09 20:31:18 +0100

    subida 2

---
+++ REPAS/exercici5.py
@@ -0,0 +1,8 @@
+
+
+num=int(input('Fica un numero: '))
+
+if num % 2 == 0:
+    print(num,'es parell')
+else:
+    print(num, 'es senar')
---
+++ REPAS/exercici8.py
@@ -0,0 +1,10 @@
+cadena = str(input('Posa entre 1 i 3 paraules: '))
+
+print('Esto es la palabra del usuario {cadena}')
+print('Total de caracters: ',len(cadena))
+print(cadena[0])
+print(cadena[-1])
+    
+
+
+
